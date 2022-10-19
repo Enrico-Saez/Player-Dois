217c018c7d9dfa748ca544a02436a9ef9a6e62dc
enric0-s <enricoricsaez@gmail.com>
2022-10-18 23:52:53 -0300

    Criei funcoes para cadastro de usuario e leitura do banco

--- CRUD_usuarios.js
+++ CRUD_usuarios.js
@@ -1,7 +1,47 @@
-//Create
+const fs = require('fs');
 
-//Read
 
+// Create
+function criarUsuario(chave, senha, nome, sexo, idade) {
+    let dados = JSON.parse(fs.readFileSync('banco_usuarios.json'));
+    dados[chave] = {
+        "senha": senha,
+        "nome": nome,
+        "sexo": sexo,
+        "idade": idade,
+        "bio": "",
+        "foto": "",
+        "avaliacao_hab": 0,
+        "avaliacao_simp": 0,
+        "lista_jogos": [],
+        "lista_plat": [],
+        "pessoas_com_conversa": [
+            ""
+        ],
+        "possui_novas_mensagens": false,
+        "mensagens": {
+            "nome": [
+                ""
+            ]
+        }
+    }
+    fs.writeFileSync('banco_usuarios.json',JSON.stringify(dados, null, 2));
+}
+
+
+
+// Read
+function getSenhaUsuario(chave) {
+    let dados = fs.readFileSync('banco_usuarios.json');
+    return JSON.parse(dados)[chave]["senha"];
+}
+
+function getNomeUsuario(chave) {
+    let dados = fs.readFileSync('banco_usuarios.json');
+    return JSON.parse(dados)[chave]["nome"];
+}
+
+criarUsuario("enricosaez", 123456, "EnricoSaez", "masculino", 18)
 //Update
 
 //Delete
